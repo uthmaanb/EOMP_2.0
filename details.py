380a6db325a46c7ba4990a2c486b1f784275735f
Uthmaan <uthmaanbreda@gmail.com>
2021-06-18 01:18:10 +0200

    pic in login

--- details.py
+++ details.py
@@ -9,8 +9,14 @@ import rsaidnumber
 import random
 
 root = Tk()
-root.geometry('450x500')
+root.geometry('400x450')
 root.title('LOGIN')
+root.config(bg='#fdcb08')
+
+looter = PhotoImage(file="lottop2.png")
+img = Label(root, image=looter)
+img.place(x=60, y=35)
+img.config(bg='#fdcb08')
 
 # StringVar
 result = StringVar()
@@ -20,20 +26,20 @@ class Login:
     
     def __init__(self, master):
         
-        self.user_lab = Label(master, text='Name:').place(x=10, y=30)
+        self.user_lab = Label(master, text='Name:').place(x=10, y=160)
         self.user_ent = Entry(master)
-        self.user_ent.place(x=150, y=30)
+        self.user_ent.place(x=150, y=160)
         self.user_ent.focus()
-        self.email_lab = Label(master, text='Email:').place(x=10, y=80)
+        self.email_lab = Label(master, text='Email:').place(x=10, y=200)
         self.email_ent = Entry(master)
-        self.email_ent.place(x=150, y=80)
-        self.id_lab = Label(master, text='ID Number:').place(x=10, y=130)
+        self.email_ent.place(x=150, y=200)
+        self.id_lab = Label(master, text='ID Number:').place(x=10, y=240)
         self.id_ent = Entry(master)
-        self.id_ent.place(x=150, y=130)
-        self.results = Label(master, text='', textvariable=result).place(x=150, y=230)
-        self.log_btn = Button(master, text='Login', command=self.id_info).place(x=150, y=180)
-        self.exit_btn = Button(master, text='Exit').place(x=130, y=280)
-        self.clr_btn = Button(master, text='Clear').place(x=230, y=280)
+        self.id_ent.place(x=150, y=240)
+        # self.results = Label(master, text='', textvariable=result).place(x=150, y=230)
+        self.log_btn = Button(master, text='Login', command=self.id_info).place(x=200, y=280)
+        self.exit_btn = Button(master, width=5, text='Exit').place(x=150, y=350)
+        self.clr_btn = Button(master, text='Clear').place(x=250, y=350)
 
     def email_info(self):
         expr = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
